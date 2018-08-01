9210d9dd9b110efc72db3987f298a4f89769d16a
Alekay <alekaymusic@gmail.com>
2018-07-31 20:02:20 -0400

    match cards functionality

--- js/app.js
+++ js/app.js
@@ -5,6 +5,10 @@ let cards = [...card];
 // Deck of all cards in game
 const deck = document.getElementById('card-deck');
 
+// move variables
+let moves = 0;
+let counter = document.querySelector('.moves');
+
 // matched card variable
 let matchedCard = document.getElementsByClassName('match');
 
@@ -12,10 +16,10 @@ let matchedCard = document.getElementsByClassName('match');
 let openedCards = [];
 
 // shuffle cards, take parameter array, return shuffled array
-function shuffle(array) {
+function shuffle(array){
     let currentIndex = array.length, temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
+    while (currentIndex !== 0){
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
         temporaryValue = array[currentIndex];
@@ -27,15 +31,15 @@ function shuffle(array) {
 }
 
 // startGame function that shuffles cards and makes deck of all cards
-function startGame() {
+function startGame(){
     // create variable for shuffled cards that uses 'shuffle array' method, shuffling the 'cards' array
     cards = shuffle(cards);
     // loop through for the length of the shuffled cards array
-    for (let i = 0; i < cards.length; i++)  {
+    for (let i = 0; i < cards.length; i++){
         deck.innerHTML = "";
         // 1. use forEach function (which takes function as input) on empty array
         // 2. use .call to take first argument and replace .this inside of the function, function takes arg (item)
-        [].forEach.call(cards, function(item) {
+        [].forEach.call(cards, function(item){
             // 3. append item to the deck of cards
             // this is where the actual shuffle happens
             deck.appendChild(item);
@@ -50,7 +54,7 @@ document.body.onload = startGame();
 
 // display the card's symbol (put this functionality in another function that you call from this one)
 // set function to toggle card's classes - will be called upon by event listener when clicked
-const displayCard = function() {
+const displayCard = function(){
     // Toggle all 3 attributes that need to change at the same time
     // 'this' is set in the window creating a global variable within the function
     this.classList.toggle('open'); 
@@ -60,7 +64,7 @@ const displayCard = function() {
 
 // this gives the primary toggle for the cards
 // loop through cards array
-for (let i = 0; i < cards.length; i++) {
+for (let i = 0; i < cards.length; i++){
     // add event listener to each card in cards array as it loops through the array of cards
     // pass 'displayCard' function to run when the click hapens
     cards[i].addEventListener('click', displayCard);
@@ -82,7 +86,7 @@ function cardOpen(){
 }
 
 // + if the cards do match, lock the cards in the open position
-function matched() {
+function matched(){
     openedCards[0].classList.add('match', 'disabled');
     openedCards[1].classList.add('match', 'disabled');
     openedCards[0].classList.remove('show', 'open');
@@ -91,7 +95,7 @@ function matched() {
 }
 
 // + if the cards do not match, remove the cards from the list and hide the card's symbol 
-function unmatched() {
+function unmatched(){
     openedCards[0].classList.add('unmatched');
     openedCards[1].classList.add('unmatched');
     disable();
@@ -104,22 +108,26 @@ function unmatched() {
 }
 
 // disable cards when not matched
-function disable() {
+function disable(){
     Array.prototype.filter.call(cards, function(card) {
         card.classList.add('disabled');
     });
 }
 
-function enable() {
-    Array.prototype.filter.call(cards, function(card) {
+function enable(){
+    Array.prototype.filter.call(cards, function(card){
         card.classList.remove('disabled');
-        for (let i = 0; i < matchedCard.length; i++) {
+        for (let i = 0; i < matchedCard.length; i++){
             matchedCard[i].classList.add('disabled');
         }
     });
 }
-    // + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
-    // + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+// + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+function moveCounter(){
+    moves++;
+    counter.innerHTML = moves;
+}
+// + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
 
 // loop to add event listeners to each card
 for (let i = 0; i < cards.length; i++){
